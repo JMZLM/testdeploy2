74e7d2d47f29b5cb84539f804a6e338f3af15c56
JMZLM <109211163+JMZLM@users.noreply.github.com>
2024-11-26 16:49:16 +0800

    Update app.py

--- app.py
+++ app.py
@@ -152,4 +152,4 @@ def detect_emotion():
 # Start Flask app
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 8080))  # Default to port 8080 if PORT is not set
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(debug=False, host='0.0.0.0', port=port)
